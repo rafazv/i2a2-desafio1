28a1bd57154b351b7f115d7f8a070a05e539c795
Rafaela Lima <ral@icomp.ufam.edu.br>
2023-07-07 19:07:18 -0400

    Fix bugs

--- desafio1/esqueci-minha-senha.js
+++ desafio1/esqueci-minha-senha.js
@@ -1,15 +1,17 @@
-document.getElementById("forgotPasswordForm").addEventListener("submit", function(event) {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("forgotPasswordForm").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-  var email = document.getElementById("email").value;
+    var email = document.getElementById("email").value;
 
-  if (email) {
-    // Simulando envio do link de redefinição para o email
-    alert("Um link para redefinição de senha foi enviado para o email informado: " + email);
-    
-    // Redirecionando para a página de login
-    window.location.href = "login.html";
-  } else {
-    alert("Por favor, informe um email válido.");
-  }
+    if (email) {
+      // Simulando envio do link de redefinição para o email
+      alert("Um link para redefinição de senha foi enviado para o email informado: " + email);
+      
+      // Redirecionando para a página de login
+      window.location.href = "login.html";
+    } else {
+      alert("Por favor, informe um email válido.");
+    }
+  });
 });
--- desafio1/login.js
+++ desafio1/login.js
@@ -1,13 +1,15 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-  if (username && password) {
-    // Aqui você pode redirecionar para a página de produtos
-    window.location.href = "produtos.html";
-  } else {
-    alert("Por favor, preencha todos os campos.");
-  }
+    if (username && password) {
+      // Aqui você pode redirecionar para a página de produtos
+      window.location.href = "produtos.html";
+    } else {
+      alert("Por favor, preencha todos os campos.");
+    }
+  });
 });
